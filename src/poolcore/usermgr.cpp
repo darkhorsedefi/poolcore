57fa4eb2d3e7efc502e82441196fb6172a92d5b1
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-02-04 17:32:40 -0800

    Test

--- src/poolcore/usermgr.cpp
+++ src/poolcore/usermgr.cpp
@@ -56,14 +56,14 @@ bool UserManager::sendMail(const std::string &login, const std::string &emailAdd
     error = "smtp_client_create_error";
     return false;
   }
-  /*
+  
   std::string EMailText;
   std::string activationLink = BaseCfg.PoolHostProtocol + "://";
   activationLink.append(BaseCfg.PoolHostAddress);
   activationLink.append(linkPrefix);
   activationLink.append(actionId.ToString());
 
-  EMailText.append("Content-Type: text/html; charset=\"ISO-8859-1\";\r\n");
+  EMailText.append("Content-Type: text/html; charset=\"charset=UTF-8\";\r\n");
   EMailText.append("This email generated automatically, please don't reply.\r\n");
   EMailText.append(mainText);
   EMailText.append(" visit <a href=\"");
@@ -71,8 +71,8 @@ bool UserManager::sendMail(const std::string &login, const std::string &emailAdd
   EMailText.append("\">");
   EMailText.append(activationLink);
   EMailText.append("</a>\r\n");
-  */
 
+  /*
   std::string EMailText;
   std::string activationLink = BaseCfg.PoolHostProtocol + "://";
   activationLink.append(BaseCfg.PoolHostAddress);
@@ -86,11 +86,12 @@ bool UserManager::sendMail(const std::string &login, const std::string &emailAdd
   EMailText.append("Subject: " + emailTitlePrefix + " " + BaseCfg.PoolName + "\r\n");
   EMailText.append("\r\n");
 
-  //EMailText.append("<html><body>");
+  EMailText.append("<html><body>");
   EMailText.append("<p>This email was generated automatically, please don't reply.</p>");
   EMailText.append("<p>" + mainText + "</p>");
   EMailText.append("<p>Visit <a href=\"" + activationLink + "\">" + activationLink + "</a></p>");
-  //EMailText.append("</body></html>");
+  EMailText.append("</body></html>");
+  */
 
   int result = ioSmtpSendMail(client,
                               SMTP.ServerAddress,
